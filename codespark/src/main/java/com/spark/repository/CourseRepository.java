a0e9e5bf0bb0cc106282e569354cbe5a891fb33a
jinagayo <jinayoon90@gmail.com>
2025-07-23 19:24:32 +0900

    ..

--- codespark/src/main/java/com/spark/repository/CourseRepository.java
+++ codespark/src/main/java/com/spark/repository/CourseRepository.java
@@ -3,6 +3,8 @@ package com.spark.repository;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -17,6 +19,12 @@ public interface CourseRepository extends JpaRepository<ClassEntity, String> {
             "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id",
 	    nativeQuery = true)
 	List<Map<String, Object>> findAllClass();
+	
+	//기본
+	Page<ClassEntity> findByTeacherId(String teacherId, Pageable pageable);
+	
+	//검색
+	Page<ClassEntity> findByTeacherIdAndTitleContainingIgnoreCase(String teacherId,  String title, Pageable pageable);
 
 
 }
--- codespark/src/main/java/com/spark/service/CourseService.java
+++ codespark/src/main/java/com/spark/service/CourseService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -26,14 +29,30 @@ public class CourseService {
     @Autowired
     private CourseRepository courseRepo;
 
-    CourseService(JoinController joinController) {
+    CourseService(JoinController joinController, CourseRepository courseRepo) {
         this.joinController = joinController;
+        this.courseRepo = courseRepo;
     }
 
 	public ResponseEntity<?> getAllCourses() {
 		List<Map<String, Object>>  CourseList = courseRepo.findAllClass();
 		
 	    return ResponseEntity.ok(CourseList);
+	}
+
+
+	// 강사별 강의+검색+페이징
+	public Page<ClassEntity> getCoursesByTeacher(String teacherId, String search, Pageable pageable) {
+		return null;
+//		if(search != null && !search.trim().isEmpty()) {
+//			return courseRepo
+//					 .findByTeacherIdAndTitleContainingIgnoreCase(teacherId, search, pageable)
+//					.map(ClassEntity::fromEntity);
+//		} else {
+//			return courseRepo
+//					.findByTeacherId(teacherId, pageable)
+//					.map(ClassEntity::fromEntity);
+//		}
 	}
 
 
