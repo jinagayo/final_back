77624cd06f3e5cd6b8b6d61365aa5a75ab461bef
twochaeone <lee00cw@gmail.com>
2025-07-28 11:57:09 +0900

    강의 승인/반려 완료

--- codespark/src/main/java/com/spark/repository/CourseRepository.java
+++ codespark/src/main/java/com/spark/repository/CourseRepository.java
@@ -22,27 +22,31 @@ public interface CourseRepository extends JpaRepository<ClassEntity, Integer> {
 	//수강신청 가능한 목록 조회
 	@Query(value = "SELECT c.class_id as classId, c.name as name, c.detail as detail, c.price as price, c.intro as intro, c.mark as mark, c.img as img, u.name as teacher, "
 			+ "com.name as subject, c.state as state, c.created_at as createdAt, c.updated_at as updatedAt,  c.created_by as createdBy, c.updated_by as updatedBy  " + 
-            "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id where c.state='STA001'",
+            "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id where c.state='STA001' AND c.is_active = true",
 	    nativeQuery = true)
 	List<ClassInfoDTO> findAllClass();
 	
 	//강사의 강의 신청 목록 조회
 	@Query(value = "SELECT c.class_id as classId, c.name as name, c.detail as detail, c.price as price, c.intro as intro, c.mark as mark, "
 			+ "c.img as img, u.name as teacher, com.name as subject, c.state as state ,c.created_at as createdAt, c.updated_at as updatedAt, c.updated_by as updatedBy, c.created_by as createdBy " +
-            "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id where teach_id=:teachId",
+            "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id where teach_id=:teachId AND c.is_active = true",
 	    nativeQuery = true)
 	List<ClassInfoDTO> findByTeachId(@Param("teachId")String teachId);
 	
 	//검색
 	Page<ClassEntity> findByTeachIdAndNameContainingIgnoreCase(String teachId, String name, Pageable pageable);
 
-	@Query(value = "SELECT c.class_id as classId, c.name as name, c.detail, c.price, c.intro, c.mark, c.img, u.name as teacher, com.name as SUBJECT , t.career AS career, t.introduce AS introduce\r\n"
-			+ "FROM `class` c\r\n"
-			+ " JOIN `user` u ON c.teach_id = u.user_id "
-			+ " JOIN `common` com ON c.sub_id = com.com_id "
-			+ " JOIN `teacher` t ON c.teach_id = t.teach_id  where class_id= :classId",
-	    nativeQuery = true)
-	List<Map<String, Object>> ClassDetail(@Param("classId")String classId);
+	@Query(value = "SELECT c.class_id as classId, c.name as name, c.detail, c.price, c.intro, c.mark, c.img, "
+	        + "u.name as teacher, com.name as SUBJECT , t.career AS career, "
+	        + "t.introduce AS introduce, c.state state "
+	        + "FROM `class` c "
+	        + "JOIN `user` u ON c.teach_id = u.user_id "
+	        + "JOIN `common` com ON c.sub_id = com.com_id "
+	        + "JOIN `teacher` t ON c.teach_id = t.teach_id "
+	        + "WHERE c.class_id = :classId AND c.is_active = true",  // 🔥 여기 추가!
+	        nativeQuery = true)
+	List<Map<String, Object>> ClassDetail(@Param("classId") String classId);
+
 
 	@Query(value = "SELECT * FROM subject_review  WHERE class_id = :classId",nativeQuery = true)
 	List<SubjectReviewEntity> findReview(@Param("classId") String classId);
@@ -50,7 +54,7 @@ public interface CourseRepository extends JpaRepository<ClassEntity, Integer> {
 	//관리자 입장에서 강의 목록 조회
 	@Query(value = "SELECT c.class_id as classId, c.name as name, c.detail as detail, c.price as price, c.intro as intro, c.mark as mark, c.img as img, u.name as teacher, "
 			+ "com.name as subject, c.state as state, c.created_at as createdAt, c.updated_at as updatedAt,  c.created_by as createdBy, c.updated_by as updatedBy  " + 
-            "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id ",
+            "FROM `class` c JOIN `user` u ON c.teach_id = u.user_id JOIN `common` com ON c.sub_id = com.com_id where c.is_active = true",
     	    nativeQuery = true)
 	List<ClassInfoDTO> findAllRequest();
 
